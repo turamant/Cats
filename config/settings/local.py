da77562efab403dd4373b6a63a284757a0586446
turamant <60545309+turamant@users.noreply.github.com>
2020-07-20 01:04:34 +0300

    Update local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,7 +8,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="n1LWYMLN4NAvqsCKSpaUOObr2K7aQDyzXwQaFuDvFtBYFSjOx41rkGmJ647wsKwo",
+    default="xxx",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
