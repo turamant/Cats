fb52c84c409793b395ca9b239a91ab56b399163e
turamant <60545309+turamant@users.noreply.github.com>
2020-07-20 16:55:44 +0300

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -6,16 +6,15 @@ from posts.views import PostListView, PostDetailView, TagListView, TagDetailView
 urlpatterns = [
     path('tags/', TagListView.as_view(), name='tags_list'),
     path('tags/create/',TagCreate.as_view(), name='tag_create'),
-    path('tags/<str:url>/', TagDetailView.as_view(), name='tag_detail'),
-    path('tags/<str:url>/update/', TagUpdate.as_view(), name='tag_update'),
-    path('tags/<str:url>/delete/', TagDelete.as_view(), name='tag_delete'),
+    path('tags/<slug:url>/', TagDetailView.as_view(), name='tag_detail'),
+    path('tags/<slug:url>/update/', TagUpdate.as_view(), name='tag_update'),
+    path('tags/<slug:url>/delete/', TagDelete.as_view(), name='tag_delete'),
 
     path('posts/', PostListView.as_view(), name='post_list'),
     path('posts/create/',PostCreate.as_view(), name='post_create'),
 
-    path('posts/<str:url>/delete/', post_delete, name='post_delete'),
-    path('posts/<str:url>/', PostDetailView.as_view(), name='post_detail'),
-    #path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
+    path('posts/<slug:url>/delete/', post_delete, name='post_delete'),
+    path('posts/<slug:url>/', PostDetailView.as_view(), name='post_detail'),
     path('posts/<slug:url>/update/', PostUpdate.as_view(), name='post_update'),
     path('search/', search, name='search'),
 
